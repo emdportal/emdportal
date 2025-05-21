db7b4452bbb05c8eb89d0af7ba1ca28ea8df325f
itsxrp <itsxrproms@gmail.com>
2025-05-21 13:19:13 +0500

    added flash login

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from html import escape
 import logging
+from flask_login import current_user, login_required  # Add Flask-Login
+
 
 # Load environment variables
 load_dotenv()
